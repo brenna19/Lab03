f2e2225c231c863da60a079151c9dc0dcc331b77
Brenna Clement <LE1943832@crc-lennox.qc.ca>
2020-10-13 20:54:25 -0400

    Add project files.

---
+++ Lab03/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Lab03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.movieListBox = new System.Windows.Forms.ListBox();
+            this.genreListBox = new System.Windows.Forms.ListBox();
+            this.addMovieButton = new System.Windows.Forms.Button();
+            this.deleteMovieButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(331, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Movies:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Genre:";
+            // 
+            // movieListBox
+            // 
+            this.movieListBox.FormattingEnabled = true;
+            this.movieListBox.ItemHeight = 16;
+            this.movieListBox.Location = new System.Drawing.Point(334, 66);
+            this.movieListBox.Name = "movieListBox";
+            this.movieListBox.Size = new System.Drawing.Size(319, 356);
+            this.movieListBox.TabIndex = 2;
+            // 
+            // genreListBox
+            // 
+            this.genreListBox.FormattingEnabled = true;
+            this.genreListBox.ItemHeight = 16;
+            this.genreListBox.Location = new System.Drawing.Point(23, 185);
+            this.genreListBox.Name = "genreListBox";
+            this.genreListBox.Size = new System.Drawing.Size(272, 84);
+            this.genreListBox.TabIndex = 3;
+            // 
+            // addMovieButton
+            // 
+            this.addMovieButton.Location = new System.Drawing.Point(677, 197);
+            this.addMovieButton.Name = "addMovieButton";
+            this.addMovieButton.Size = new System.Drawing.Size(93, 45);
+            this.addMovieButton.TabIndex = 4;
+            this.addMovieButton.Text = "Add Movie";
+            this.addMovieButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteMovieButton
+            // 
+            this.deleteMovieButton.Location = new System.Drawing.Point(677, 273);
+            this.deleteMovieButton.Name = "deleteMovieButton";
+            this.deleteMovieButton.Size = new System.Drawing.Size(93, 46);
+            this.deleteMovieButton.TabIndex = 5;
+            this.deleteMovieButton.Text = "Delete Movie";
+            this.deleteMovieButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.Controls.Add(this.deleteMovieButton);
+            this.Controls.Add(this.addMovieButton);
+            this.Controls.Add(this.genreListBox);
+            this.Controls.Add(this.movieListBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "XML Movie Maker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox movieListBox;
+        private System.Windows.Forms.ListBox genreListBox;
+        private System.Windows.Forms.Button addMovieButton;
+        private System.Windows.Forms.Button deleteMovieButton;
+    }
+}
+
---
+++ Lab03/Form2.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Lab03
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.yearTextBox = new System.Windows.Forms.TextBox();
+            this.lengthTextBox = new System.Windows.Forms.TextBox();
+            this.directorTextBox = new System.Windows.Forms.TextBox();
+            this.ratingTextBox = new System.Windows.Forms.TextBox();
+            this.editButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(33, 113);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(300, 400);
+            this.imageBox.TabIndex = 0;
+            this.imageBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Year:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Length:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Director:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(385, 434);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Rating:";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(155, 28);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(468, 22);
+            this.titleTextBox.TabIndex = 5;
+            this.titleTextBox.Text = "TITLE";
+            this.titleTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // yearTextBox
+            // 
+            this.yearTextBox.Location = new System.Drawing.Point(496, 124);
+            this.yearTextBox.Name = "yearTextBox";
+            this.yearTextBox.Size = new System.Drawing.Size(260, 22);
+            this.yearTextBox.TabIndex = 6;
+            // 
+            // lengthTextBox
+            // 
+            this.lengthTextBox.Location = new System.Drawing.Point(496, 222);
+            this.lengthTextBox.Name = "lengthTextBox";
+            this.lengthTextBox.Size = new System.Drawing.Size(260, 22);
+            this.lengthTextBox.TabIndex = 7;
+            // 
+            // directorTextBox
+            // 
+            this.directorTextBox.Location = new System.Drawing.Point(496, 323);
+            this.directorTextBox.Name = "directorTextBox";
+            this.directorTextBox.Size = new System.Drawing.Size(260, 22);
+            this.directorTextBox.TabIndex = 8;
+            // 
+            // ratingTextBox
+            // 
+            this.ratingTextBox.Location = new System.Drawing.Point(496, 431);
+            this.ratingTextBox.Name = "ratingTextBox";
+            this.ratingTextBox.Size = new System.Drawing.Size(260, 22);
+            this.ratingTextBox.TabIndex = 9;
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(462, 490);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(108, 41);
+            this.editButton.TabIndex = 10;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(650, 490);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(106, 41);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Save and Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 556);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.ratingTextBox);
+            this.Controls.Add(this.directorTextBox);
+            this.Controls.Add(this.lengthTextBox);
+            this.Controls.Add(this.yearTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageBox);
+            this.Name = "Form2";
+            this.Text = "XML Movie Maker";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox yearTextBox;
+        private System.Windows.Forms.TextBox lengthTextBox;
+        private System.Windows.Forms.TextBox directorTextBox;
+        private System.Windows.Forms.TextBox ratingTextBox;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
